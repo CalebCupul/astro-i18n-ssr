4794fb3014aa4fa64c9d90f360ba8a7e28bd6a0c
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2024-03-21 14:59:18 -0600

    Add consts.ts file and update typings

---
+++ src/consts.ts
@@ -0,0 +1,16 @@
+import type { Domain } from "./types/global.d.ts";
+
+export const DOMAINS: readonly Domain[] = [
+  {
+    name: "Villa la Valencia",
+    url: "villalavalencia",
+  },
+  {
+    name: "Villa la Estancia",
+    url: "villalaestancia",
+  },
+  {
+    name: "Villa del Palmar",
+    url: "villadelpalmar",
+  },
+];
